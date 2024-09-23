3d3f2fc6d3e8adeed5a714ef57978af3c84def43
Agostino Calamia <calamia.tino@gmail.com>
2024-09-24 00:04:17 +0200

    update poetry

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,10 +1,22 @@
 import os
-from langchain_community.vectorstores import Chroma
+import faiss
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_openai import OpenAIEmbeddings
 
 import src.constants as con
 
-def create_vectorstore_from_documents(documents, vectorstore=Chroma(), embedding=OpenAIEmbeddings()):
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
+
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+def create_vectorstore_from_documents(documents, vectorstore=vector_store, embedding=OpenAIEmbeddings()):
     vector_db = vectorstore.from_documents(documents=documents, embedding=embedding)
 
     return vector_db
